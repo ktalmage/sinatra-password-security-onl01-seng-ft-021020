90ad60cb0d43b76e069bbeaaff366630bc37210b
Kyle Talmage <ktalmage85@gmail.com>
2020-04-03 13:22:07 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 require "./config/environment"
 require "./app/models/user"
 class ApplicationController < Sinatra::Base
@@ -69,3 +70,77 @@ class ApplicationController < Sinatra::Base
 	end
 
 end
+=======
+require "./config/environment"
+require "./app/models/user"
+class ApplicationController < Sinatra::Base
+
+	configure do
+		set :views, "app/views"
+		enable :sessions
+		set :session_secret, "password_security"
+	end
+
+	get "/" do
+		erb :index
+	end
+
+	get "/signup" do
+		erb :signup
+	end
+
+	post "/signup" do
+		user = User.new(:username => params[:username], :password => params[:password])
+		
+		if user.save
+		  redirect '/login'
+		else
+		  redirect '/failure'
+		end
+	end
+
+	get "/login" do
+	  
+		erb :login
+	end
+
+	post "/login" do
+		user = User.find_by(:username => params[:username])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			binding.pry
+			redirect "/success"
+		else
+			redirect "/failure"
+		end
+	end
+
+	get "/success" do
+		if logged_in?
+			erb :success
+		else
+			redirect "/login"
+		end
+	end
+
+	get "/failure" do
+		erb :failure
+	end
+
+	get "/logout" do
+		session.clear
+		redirect "/"
+	end
+
+	helpers do
+		def logged_in?
+			!!session[:id]
+	  end
+
+		def current_user
+		  User.find(session[:id])
+	  end
+	end
+
+end
+>>>>>>> c501121d07b8c83b1ce0392646c07a9e6cd9b0bd
